597357f2b355e197790596600f0c2c80f9f06f74
Akira <freelance0918@gmail.com>
2022-09-27 21:07:56 +0900

    add UserModel

--- reply/views.py
+++ reply/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-
+from .models import Users
 # Create your views here.
 
 
 def index(request):
     context = {}
     return render(request, 'reply/index.html', context)
+
+
+def login(request):
+    name = request.GET('name')
+    password = request.GET('pass')
+    login = Users.objects.filter(name=name, password=password)
+    return render(request, 'reply/', {})
